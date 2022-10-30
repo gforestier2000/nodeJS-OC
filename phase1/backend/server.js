6a44d0935de19be07ebbf6bd6726f4897e9b8780
Guillaume Forestier <gforestier2000@yahoo.fr>
2022-10-30 18:50:09 +0100

    add phase1/backend

---
+++ phase1/backend/server.js
@@ -0,0 +1,11 @@
+
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    res.end('Voilà la réponse du serveur !');
+});
+
+
+server.listen(process.env.PORT || 3000, ()=>{
+    console.log("Server listen on : " + 3000);
+});
